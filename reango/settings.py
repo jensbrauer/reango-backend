0a8cd7439a0c43c1691fd6686d33dcb5f7224016
Jens Brauer <jens@tyfoon.se>
2023-09-27 09:25:23 +0200

    allow coorect hosts

--- reango/settings.py
+++ reango/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://reango-48565de87753.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['reango-48565de87753.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
