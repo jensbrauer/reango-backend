4e3f64d69d4e0eba35c758b587672335bc0f2937
Jens Brauer <jens@tyfoon.se>
2023-10-06 11:37:28 +0200

    update allowed host

--- reango/urls.py
+++ reango/urls.py
@@ -2,15 +2,18 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('token/', 
-          jwt_views.TokenObtainPairView.as_view(), 
+          TokenObtainPairView.as_view(), 
           name ='token_obtain_pair'),
      path('token/refresh/', 
-          jwt_views.TokenRefreshView.as_view(), 
+          TokenRefreshView.as_view(), 
           name ='token_refresh'),
 ]
