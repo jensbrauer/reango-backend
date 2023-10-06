8a1c6cd02596612ab4ca37582c72a7881bc17557
Jens Brauer <jens@tyfoon.se>
2023-10-06 11:08:02 +0200

    debug to true

--- reango/settings.py
+++ reango/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['reango-48565de87753.herokuapp.com', '127.0.0.1', 'localhost', 'localhost:3000']
 
